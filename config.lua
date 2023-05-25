2ec81d8652760f7c9728fedda4f1061dfcfea71e
FreakkyDE <127225221+FreakkyDE@users.noreply.github.com>
2023-05-25 10:21:51 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,8 @@
+Config = {}
+
+Config.EnableDebug = true -- Enable or disable debug mode
+Config.MoneyTransportEventInterval = 60 -- Interval in seconds between event spawns
+Config.TransportVehicleModel = "stockade" -- Model name of the transporter vehicle
+Config.TransportVehicleSpawnCoords = vector3(0.0, 0.0, 0.0) -- Coordinates where the transporter spawns
+Config.TransportVehicleMarkerColor = { r = 0, g = 255, b = 0 } -- RGB color of the transporter marker
+-- More to put in
