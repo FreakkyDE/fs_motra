e347fe692e42afef93b5b05e07456e9a21fcb039
FreakkyDE <127225221+FreakkyDE@users.noreply.github.com>
2023-05-25 10:20:52 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+-- Dependency
+dependency 'qb-core'
+
+-- Specify the script entry point
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+server_script 'server.lua'
